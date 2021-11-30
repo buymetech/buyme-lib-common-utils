509008a6a6896d19cc82a1b03771ea46476c29a1
Ariel Morry <ariel@buyme.co.il>
2021-11-30 06:55:21 +0200

    Move elastic to a separate file

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,29 +1,5 @@
-import {config} from "../env.js";
-import {ApiResponse, Client} from "@elastic/elasticsearch";
-import {TransportRequestPromise} from "@elastic/elasticsearch/lib/Transport";
-
 export class Logger {
-    readonly client: Client;
-
     constructor() {
-        this.client = new Client({
-            cloud: {
-                id: config.elastic.cloud_id,
-            },
-            auth: {
-                username: config.elastic.username,
-                password: config.elastic.password
-            }
-        });
-    }
 
-    run(): TransportRequestPromise<ApiResponse> {
-        return this.client.index({
-            index: 'test-index',
-            refresh: true,
-            body: {
-                service: config.elastic.service,
-            }
-        })
     }
 }
