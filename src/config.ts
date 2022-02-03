40b6c66e34ca45f82931d9c4b0e78aa41f8f2157
Ariel Morry <ariel@buyme.co.il>
2022-02-03 20:28:40 +0200

    MIC-230 Fix type in common lib env

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const config = {
   service: process.env.SERVICE,
   log_level: process.env.LOGGING_LEVEL,
   elastic: {
-    username: process.env.ELASTIC_USERNANME,
+    username: process.env.ELASTIC_USERNAME,
     password: process.env.ELASTIC_PASSWORD,
     cloud_id: process.env.ELASTIC_CLOUD_ID,
     index: process.env.ELASTIC_INDEX,
