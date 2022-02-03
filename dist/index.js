29754ec0c36ee5c256d28caa2d6d6b250dc4babd
Ariel Morry <ariel@buyme.co.il>
2022-02-03 20:29:04 +0200

    MIC-230 Fix type in common lib env

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,12 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const logger_module_js_1 = __importDefault(
-  require('./logger/logger.module.js'),
-);
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const logger_module_js_1 = __importDefault(require("./logger/logger.module.js"));
 class Utils {
-  static logger(opt) {
-    return new logger_module_js_1.default(opt).logger;
-  }
+    static logger(opt) {
+        return new logger_module_js_1.default(opt).logger;
+    }
 }
-
 exports.default = Utils;
