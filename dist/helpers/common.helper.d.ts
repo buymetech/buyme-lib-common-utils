663f1a707201256aa743d14275afbab5ab146d19
udiatbuyme <udi@buyme.co.il>
2022-03-10 16:54:37 +0200

    MIC-313 Delete Large Properties from the logged object

--- dist/helpers/common.helper.d.ts
+++ dist/helpers/common.helper.d.ts
@@ -1,3 +1,4 @@
 export declare class CommonHelper {
     static findValInObject(object: any, key: string, del?: boolean): undefined;
+    static deleteLargeProps(object: any): void;
 }
--- dist/logger/elastic/elastic.transport.d.ts
+++ dist/logger/elastic/elastic.transport.d.ts
@@ -1,7 +1,7 @@
 import Transport, * as transportHelper from 'winston-transport';
 export declare class ElasticTransport extends Transport {
     private opts;
-    private date;
+    private data;
     constructor(opts?: transportHelper.TransportStreamOptions);
     log(data: any, callback: () => void): void;
     getLogObject(): {};
